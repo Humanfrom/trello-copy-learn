8fd06760da7e5dde8aaace9036f76333119b7f52
Humanfrom <98333122+Humanfrom@users.noreply.github.com>
2022-04-06 17:36:12 +0300

    Reinit GIT

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import "./index.less"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx'
+import {store} from "./reducers/index.js"
+import {Provider} from "react-redux"
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  , document.getElementById("root")
+)
